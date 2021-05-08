f332ca0f5b05bd648c0052f7a893c0fddfb66570
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-08 16:39:17 -0500

    added image and headline component maker functions to index.js

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 function linkSectionMaker () {
-    const containerDiv = document.createElement("div"); 
+    const linkContainerDiv = document.createElement("div"); 
     const linkDiv = document.createElement("div");
     const listUL = document.createElement("ul");
     const listLI = document.createElement("li");
     const linkA = document.createElement("a");
 
-    containerDiv.appendChild(linkDiv);
+    linkContainerDiv.appendChild(linkDiv);
     linkDiv.appendChild(listUL);   
     listUL.appendChild(listLI);
     listLI.appendChild(linkA);
 
-    return containerDiv;
+    return linkContainerDiv;
 
 }
 
+function imageMaker () {
+    const imageContainerDiv = document.createElement("div");
+    const image = document.createElement("img");
+
+    imageContainerDiv.appendChild(image);
+
+    return imageContainerDiv;
+
+}
+
+function headlineMaker () {
+    const headlineContainerDiv = document.createElement("div");
+    const headline = document.createElement("h1");
+    const headlineLink = document.createElement("a");
+
+    headlineContainerDiv.appendChild(headline);
+    headlineContainerDiv.appendChild(headlineLink);
+
+    return headlineContainerDiv;
+    
+}
